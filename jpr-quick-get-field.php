2315320e34747de7d279785942376458477a7378
John Oleksowicz <joleksow@nerdery.com>
2016-09-09 16:28:25 -0500

    Update primary plugin file to use new prefixing and class references

--- jpr-quick-get-field.php
+++ jpr-quick-get-field.php
@@ -8,24 +8,25 @@ Author: John Oleksowicz
 Author URI: http://jolekpress.com
 */
 
-require 'quick-get-classes/Getter.php';
+require 'quick-get-classes/QuickGet.php';
 require 'quick-get-classes/CacheInterface.php';
 require 'quick-get-classes/DatabaseCacher.php';
 require 'quick-get-classes/Helper.php';
 require 'quick-get-classes/WPObjectCacheCacher.php';
 
-$jpQuickGetFieldCacher = new JP\QuickGetField\DatabaseCacher();
+// Default cacher
+$jprQuickGetFieldCacher = new JP\QuickGetField\DatabaseCacher();
 
 /**
  * In case you want to use a different caching mechanism, here is a filter to do that. $dbCacher MUST implement
  * the JP\QuickGetField\CacheInterface.
  */
-$jpQuickGetFieldCacher = apply_filters('jpr_quick_get_field_cacher', $jpQuickGetFieldCacher);
+$jprQuickGetFieldCacher = apply_filters('jpr_quick_get_field_cacher', $jprQuickGetFieldCacher);
 
-$jpQuickGetFieldGetter = new JP\QuickGetField\Getter($jpQuickGetFieldCacher);
+$jprQuickGet = new JP\QuickGetField\QuickGet($jprQuickGetFieldCacher);
 
 function jpr_quick_get_field($fieldId, $postId = null) {
-    global $jpQuickGetFieldGetter;
+    global $jprQuickGet;
 
-    return $jpQuickGetFieldGetter->getField($fieldId, $postId);
+    return $jprQuickGet->getField($fieldId, $postId);
 }
