3fe7cedf7f38fb0e133d618447a7269620c1ea24
John Oleksowicz <joleksow@nerdery.com>
2016-09-11 20:46:22 -0500

    Update namespaces and add shortcode

--- jpr-quick-get-field.php
+++ jpr-quick-get-field.php
@@ -18,15 +18,49 @@ require 'quick-get-classes/WPObjectCacheCacher.php';
 $jprQuickGetFieldCacher = new JP\QuickGetField\DatabaseCacher();
 
 /**
- * In case you want to use a different caching mechanism, here is a filter to do that. $dbCacher MUST implement
- * the JP\QuickGetField\CacheInterface.
+ * In case you want to use a different caching mechanism, here is a filter to do that. The returned object MUST implement
+ * the JPR\QuickGetField\CacheInterface.
  */
 $jprQuickGetFieldCacher = apply_filters('jpr_quick_get_field_cacher', $jprQuickGetFieldCacher);
 
-$jprQuickGet = new JP\QuickGetField\QuickGet($jprQuickGetFieldCacher);
+$jprQuickGet = new JPR\QuickGetField\QuickGet($jprQuickGetFieldCacher);
 
 function jpr_quick_get_field($fieldId, $postId = null) {
     global $jprQuickGet;
 
     return $jprQuickGet->getField($fieldId, $postId);
 }
+
+/**
+ * Shortcode for efficient display of text based ACF values
+ *
+ * @param $userProvidedAtts
+ * @return string
+ */
+function jpr_quick_field_shortcode($userProvidedAtts)
+{
+    global $post;
+    $defaults = [
+        'field' => '',
+        'post_id' => $post->ID,
+    ];
+
+    $filteredAtts = shortcode_atts($defaults, $userProvidedAtts, 'jpr_quick_field');
+
+    $field = $filteredAtts['field'];
+    $post_id = $filteredAtts['post_id'];
+
+    if (empty($field)) {
+        return '';
+    }
+
+    $value = jpr_quick_get_field($field, $post_id);
+
+    if (!is_string($value)) {
+        return '';
+    }
+
+    return $value;
+}
+
+add_shortcode('jpr_quick_field', 'jpr_quick_field_shortcode');
